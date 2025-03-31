0123fd3eb961c6083fcdfd5988ac643a8d0c68f9
Burak Furkan <70804294+burakfkoc@users.noreply.github.com>
2025-03-31 14:10:28 +0300

    tamguı ve react native paketleri güncellendi

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,7 @@ module.exports = function (api) {
     api.cache(true)
     return {
       presets: ['babel-preset-expo'],
-      plugins: ['tamagui/babel-plugin'],
+      plugins: ['@tamagui/babel-plugin'],
     }
   }
   
