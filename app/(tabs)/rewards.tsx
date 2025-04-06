00ecea108e82e40d7b7f7bae2cb69dd4fc7c3dad
UnknownUser713 <esmagunay306@gmail.com>
2025-04-06 10:45:34 +0300

    Delete app/(tabs)/rewards.tsx

--- app/(tabs)/rewards.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'tamagui'
-
-export default function RewardsScreen() {
-  return (
-    <View flex={1} justifyContent="center" alignItems="center">
-      <Text fontSize="$6" fontWeight="bold">Ödüller</Text>
-    </View>
-  )
-}
